c65bf77a1682f1d839fd053075a499e8a254a0c5
Vicky Gupta <vickyguptaa7@gmail.com>
2022-08-21 21:12:58 +0530

    firebase config and redirect file

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyD7rueMENBmWWoWtD3o_4uEH8fiVQNBM_g",
+  authDomain: "enkrypt-4fbe3.firebaseapp.com",
+  projectId: "enkrypt-4fbe3",
+  storageBucket: "enkrypt-4fbe3.appspot.com",
+  messagingSenderId: "200689053681",
+  appId: "1:200689053681:web:4e17e53c2077fb3e43de50",
+  measurementId: "G-VEF4KS5CMD"
 };
 
 export default firebaseConfig;
