f7bfd3f1732ecad2e746a44458522b8b7e466827
Vicky Gupta <vickyguptaa7@gmail.com>
2022-08-24 23:36:31 +0530

    home page started

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { twMerge } from "tailwind-merge";
 
 const Home = () => {
+  const isDarkMode = useSelector((state) => state.ui.isDarkMode);
   return (
-    <div className="">
-      Home
+    <div className="mt-28 mx-auto">
+      <div className="header flex flex-col items-center justify-center">
+        <h1 className="text-[color:var(--color-primary)] text-4xl font-semibold mb-2 text-center">
+          Enkrypt
+        </h1>
+        <div className="h-[0.30rem] w-12 bg-[color:var(--color-primary)] rounded-full"></div>
+        <p
+          className={twMerge(
+            "mt-4 text-lg",
+            isDarkMode ? "text-gray-300" : "text-gray-400"
+          )}
+        >
+          Graphical Password Authentication
+        </p>
+      </div>
     </div>
   );
 };
